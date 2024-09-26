f8d8e14883c0063473476985cb912c269833ba8e
Rashik Shahjahan <rashikshahjahan@protonmail.com>
2024-09-26 15:19:20 -0400

    Added prisma

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import path from "path";
 import cors from "cors";
+import { PrismaClient} from '@prisma/client'
 
+const prisma = new PrismaClient();
 const app = express();
 app.use(cors());
 
@@ -16,38 +18,35 @@ const port = 3000;
 // In-memory storage for surveys switch to sql
 type Survey = { name:string; questions: [string]; results: [[string]] };
 
-let surveys: Survey[] = [{name:"survey",questions:["q1"],results:[["r1"]]},{name:"survey",questions:["q1"],results:[["r1"]]}];
-
-
-  app.get("/success", (req, res) => {
+app.get("/success", (req, res) => {
 
 
   });
 
-  app.post("/create", (req, res) => {
+app.post("/create", (req, res) => {
 
   });
 
-
-
-  app.get("/surveys/", (req, res) => {
+app.get("/surveys", async (req, res) => {
+    const surveys = await prisma.survey.findMany();
+    console.log(surveys);
     res.json(surveys);
   });
   
-  app.get("/surveys/:name/result", (req, res) => {
+app.get("/surveys/:name/result", (req, res) => {
  
   });
 
 
-
-  app.post("/surveys/:name/submit", (req, res) => {
+app.post("/surveys/:name/submit", (req, res) => {
 
   });
 
-  app.get("/surveys/:name/take", (req, res) => {
+app.get("/surveys/:name/take", (req, res) => {
 
  
   });
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
